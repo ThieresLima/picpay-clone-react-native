2782cc43faed193b87565f291fe5390f0c2cd331
Thieres Lima <thieresllima@gmail.com>
2020-07-29 17:04:40 -0300

    PicPay

---
+++ src/components/Banner/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { Container, Details, Img, Title, Descriptions } from './styles';
+
+import img13 from '../../images/13.png';
+
+export default function Banner() {
+    return (
+        <Container>
+            <Details>
+                <Title>Cobre um amigo</Title>
+
+                <Descriptions>
+                    Matenha suas parcerias em dia, use o PicPay para fazer suas cobranças.
+                </Descriptions>
+            </Details>
+            <Img source={img13} />
+        </Container>
+    );
+}
